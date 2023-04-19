b3ad5d5a39c594b50fc4d5ea9d2805ebff2af2a7
varunkumarrobo <114062044+varunkumarrobo@users.noreply.github.com>
2023-04-19 18:11:52 +0530

    first commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-
     
+    var player: AVAudioPlayer!
+    
+//    var tmer: Timer?
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        UIView.animate(withDuration: 0.3) {
+                sender.alpha = 0.5
+            }
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.3 ) {
+                UIView.animate(withDuration: 0.3) {
+                    sender.alpha = 1.0
+                }
+                
+                playSound(soundTrackName: sender.currentTitle ?? "c")
+            }
+        
+        
+//        func timer(){
+//            tmer = Timer.scheduledTimer(withTimeInterval: 2, repeats:false, block: {_ in
+//                sender.alpha=1
+//              UIView.animate(withDuration: 0.3) {
+//                sender.alpha = 1.0
+//               }
+//            })
+        
+    
+    func playSound(soundTrackName: String) {
+        let url = Bundle.main.url(forResource: soundTrackName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
 
+
+}
